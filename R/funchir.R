0e02eb53844f275e773993005d82725475e77701
Michael Chirico <MichaelChirico4@gmail.com>
2016-02-28 23:04:42 -0500

    adding use.x/y arguments to tile.axes

--- R/funchir.R
+++ R/funchir.R
@@ -55,11 +55,12 @@ dev.off2 <- function(typ = "pdf"){
 
 ## Multiplot axis generator
 ##   Plot #n in an MxN grid
-tile.axes <- function(n, M, N, params = list(list(),list())){
+tile.axes <- function(n, M, N, params = list(list(),list()), 
+                      use.x = TRUE, use.y = TRUE){
   #only print x axes on the last row
-  if (n > (M - 1) * N | M == 1) do.call("axis", c(side = 1, params[[1]]))
+  if ((n > (M - 1) * N | M == 1) & use.x) do.call("axis", c(side = 1, params[[1]]))
   #only print y axes on the first column
-  if (n %% N == 1 | N == 1) do.call("axis", c(side = 2, params[[2]]))
+  if ((n %% N == 1 | N == 1) & use.y) do.call("axis", c(side = 2, params[[2]]))
 }
 
 ## Methods for getting relative position in axes
