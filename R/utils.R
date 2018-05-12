04372814c1546e9cabb5f50cd08f2b54f4f81cd7
Michael Chirico <michael.chirico@grabtaxi.com>
2018-05-12 12:29:38 +0800

    code tidying

--- R/utils.R
+++ R/utils.R
@@ -2,9 +2,9 @@
 create_quantiles <- function(x, num, right = FALSE, na.rm = FALSE,
                              include.lowest = TRUE, labels = 1:num){
   uniq_Qs = unique(quantile(x, probs = 0:num/num, na.rm = na.rm))
-  if (length(uniq_Qs) - 1L != length(labels) && !is.null(labels)) 
+  if (length(uniq_Qs) - 1L != length(labels) && !is.null(labels))
     stop('Overlapping quantiles. Please provide ', length(uniq_Qs) - 1L, ' labels.')
-  cut(x, breaks = uniq_Qs, labels = labels, right = right, 
+  cut(x, breaks = uniq_Qs, labels = labels, right = right,
       include.lowest = include.lowest)
 }
 
@@ -27,14 +27,14 @@ dol.form <- function(x, dig = 0L, suff = "", tex = FALSE){
   div <- c(1, "k"=1e3, "m"=1e6, "b"=1e9)
   idx <- which(names(div) == suff)
   paste0(neg, if (tex) "\\", "$",
-         prettyNum(round(abs(x)/div[idx], digits = dig), 
+         prettyNum(round(abs(x)/div[idx], digits = dig),
                    big.mark = ",", scientific = FALSE), suff)
 }
 
 # Convert numbers to strings OF SPECIFIED LENGTH
 #   Convenient for getting c("99","00") from 99:100
 ntostr <- function(n, dig = 2L){
-  sprintf("%0" %+% dig %+% "d", n %% 10^dig)
+  sprintf(sprintf("%%0%dd", dig), n %% 10L^dig)
 }
 
 # Condensed cleaning of work space at
@@ -47,8 +47,8 @@ ntostr <- function(n, dig = 2L){
 #   if (detach.packages) {
 #     base.pkg = "package:" %+% c("stats", "graphics", "grDevices",
 #                                 "utils", "datasets", "methods", "base")
-#     invisible(lapply(setdiff(grep("^package:", 
-#                                   search(), value = TRUE), base.pkg), 
+#     invisible(lapply(setdiff(grep("^package:",
+#                                   search(), value = TRUE), base.pkg),
 #                      detach, character.only = TRUE, unload = TRUE))
 #   }
 # }
@@ -57,12 +57,12 @@ ntostr <- function(n, dig = 2L){
 #   (for tracking package versions over time)
 write.packages <- function(file) {
   si = sessionInfo()
-  desc_fields = c('Version', 'Depends', 'Imports', 'Suggests', 
+  desc_fields = c('Version', 'Depends', 'Imports', 'Suggests',
                   'License', 'URL', 'Packaged', 'Built')
   desc_pad = function(desc) `names<-`(desc[desc_fields], desc_fields)
   locale_list = function(loc_str) {
-    pairs = do.call(rbind, strsplit(strsplit(Sys.getlocale(), ';', 
-                                             fixed = TRUE)[[1L]], '=', 
+    pairs = do.call(rbind, strsplit(strsplit(Sys.getlocale(), ';',
+                                             fixed = TRUE)[[1L]], '=',
                                     fixed = TRUE))
     `names<-`(as.list(pairs[ , 2L]), pairs[ , 1L])
   }
@@ -85,11 +85,11 @@ write.packages <- function(file) {
 # Embed the matrix mat in a larger matrix by
 #   placing the top-left element of mat at the supplied
 #   position (m,n).
-embed.mat <- function(mat, M = nrow(mat), N = ncol(mat), 
+embed.mat <- function(mat, M = nrow(mat), N = ncol(mat),
                       m = 1L, n = 1L, fill = 0L) {
-  if (m > M || n > N) 
+  if (m > M || n > N)
     stop("Supplied starting position outside supplied enclosing matrix bounds")
-  if ((end1 <- m + nrow(mat) - 1L) > M || 
+  if ((end1 <- m + nrow(mat) - 1L) > M ||
       (end2 <- n + ncol(mat) - 1L) > N){
     stop("Supplied matrix too large for supplied enclosing matrix")
   }
@@ -98,21 +98,21 @@ embed.mat <- function(mat, M = nrow(mat), N = ncol(mat),
   out
 }
 
-# Quick scan of code for whether the 
+# Quick scan of code for whether the
 #   packages loaded are actually used
 stale_package_check = function(file_name) {
   code = readLines(file_name)
   #should fix this to match require calls too...
-  pkg_used = 
+  pkg_used =
     #also misses lines with multiple package calls
-    gsub('.*library\\(["]?([^)]*)["]?\\).*', '\\1', 
+    gsub('.*library\\(["]?([^)]*)["]?\\).*', '\\1',
          grep('library', code, value = TRUE, fixed = TRUE))
   for (pkg in unique(pkg_used)) {
     fns = getNamespaceExports(pkg)
     # issue: short function names like D are likely
     #   to be matched even if unused as a function --
     #   this check could be more sophisticated
-    used = names(which(sapply(fns, function(x) 
+    used = names(which(sapply(fns, function(x)
       any(grepl(x, code, fixed = TRUE)))))
     if (length(used)) cat('\nFunctions matched from package ', pkg, ': ',
                           paste(used, collapse = ', '), sep = '')
@@ -129,14 +129,14 @@ get_age <- function(birthdays, ref_dates){
   .BY = NULL
   x <- data.table(bday <- unclass(birthdays),
                   rem = ((ref <- unclass(ref_dates)) - bday) %% 1461)
-  x[ , cycle_type := 
+  x[ , cycle_type :=
        foverlaps(data.table(start = bdr <- bday %% 1461, end = bdr),
-                 data.table(start = c(0, 59, 424, 790, 1155), 
-                            end = c(58, 423, 789, 1154, 1460), 
+                 data.table(start = c(0, 59, 424, 790, 1155),
+                            end = c(58, 423, 789, 1154, 1460),
                             val = c(3L, 2L, 1L, 4L, 3L),
                             key = "start,end"))$val]
   I4 <- diag(4L)[ , -4L]
-  x[ , extra := 
+  x[ , extra :=
        foverlaps(data.table(start = rem, end = rem),
                  data.table(start = st <- cumsum(c(0, rep(365, 3L) +
                                                      I4[.BY[[1L]], ])),
@@ -154,8 +154,8 @@ quick_year = function(dates) {
   1970L + 4L * quadrennia + rem_yrs
 }
 
-quick_yday = function(dates) 
-  ((((unclass(dates) %% 1461L) %% 1096L) %% 730L) %% 365L) + 
+quick_yday = function(dates)
+  ((((unclass(dates) %% 1461L) %% 1096L) %% 730L) %% 365L) +
   365L * (unclass(dates) == 1095L) + 1L
 
 #Month days in the quadrennial cycle
@@ -168,12 +168,12 @@ quick_yday = function(dates)
                 1L:31L, 1L:28L, 1L:31L, 1L:30L, 1L:31L, 1L:30L,
                 1L:31L, 1L:31L, 1L:30L, 1L:31L, 1L:30L, 1L:31L)
 
-quick_mday = function(dates) 
+quick_mday = function(dates)
   .mday1461__[1L + unclass(dates) %% 1461L]
 
 # Tired of over-using as.Date everywhere...
 D <- function(...){
-  if (is.null(names(dl <- list(...)))) 
+  if (is.null(names(dl <- list(...))))
     return(do.call("as.Date", list(do.call("c", dl))))
   do.call("as.Date", c(list(do.call("c", dl[nm <- names(dl) == ""])), dl[!nm]))
 }
