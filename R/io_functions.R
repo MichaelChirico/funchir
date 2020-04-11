3ecbcf18ed11c80f3cdda18f1b33a6f599cb0243
Michael Chirico <michael.chirico@grabtaxi.com>
2020-04-12 00:37:55 +0800

    remove table2

--- R/io_functions.R
+++ R/io_functions.R
@@ -10,23 +10,6 @@ abbr_to_colClass <- function(inits, counts){
   rep(unname(lookup[x]), strsplit(counts, split = "")[[1L]])
 }
 
-# Table & regression convenience functions ####
-## Extending base table with some common options
-table2<-function(..., dig = if (prop) 2L else NULL,
-                 prop = FALSE, ord = FALSE, pct = FALSE){
-  if (ord == "dec"){ dec <- TRUE; ord <- TRUE} else dec <- FALSE
-  dots <- list(...)
-  args <- names(dots) %in% names(formals(prop.table))
-  tab <- if (prop) {
-    do.call('prop.table',c(list(
-      do.call('table', if (length(args)) dots[!args] else dots)),
-      dots[args]))
-  } else do.call('table', list(...))
-  if (ord) tab<-tab[order(tab, decreasing = dec)]
-  if (pct) tab <- 100 * tab
-  if (is.null(dig)) tab else round(tab, digits = dig)
-}
-
 sanitize2 <- function(str) {
   result <- str
   result <- gsub("\\\\", "SANITIZE.BACKSLASH", result)
