8351f9b76215bf6d116765905f673c09fa594f5a
Michael Chirico <michael.chirico@grabtaxi.com>
2020-04-12 16:58:34 +0800

    still small numeric difference on windows

--- tests/testthat/test-plot-utils.R
+++ tests/testthat/test-plot-utils.R
@@ -4,9 +4,9 @@ test_that('device-units-to-inches functions work', {
   on.exit(dev.off())
   grDevices::png(tempfile())
   plot(NA, xlim=0:1, ylim=0:1)
-  expect_equal(xdev2in(), 5.02469135802469)
-  expect_equal(ydev2in(), 4.46913580246914)
-  expect_equal(xydev2in(), c(5.02469135802469, 4.46913580246914))
+  expect_equal(xdev2in(), 5.02469135802469, tolerance = 1e-4)
+  expect_equal(ydev2in(), 4.46913580246914, tolerance = 1e-4)
+  expect_equal(xydev2in(), c(5.02469135802469, 4.46913580246914), tolerance = 1e-4)
 
   for (ii in 1:4) expect_invisible(tile.axes(ii, 2L, 2L))
 })
