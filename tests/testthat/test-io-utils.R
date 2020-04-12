3fd665ef2c9226076e72f167517778bd7ffd201c
Michael Chirico <michael.chirico@grabtaxi.com>
2020-04-12 16:37:03 +0800

    need to add new scripts

---
+++ tests/testthat/test-io-utils.R
@@ -0,0 +1,18 @@
+context('I/O Utility Functions')
+
+test_that('abbr_to_colClass works', {
+  expect_equal(
+    abbr_to_colClass('bcflinDtds', '1234567891'),
+    rep(
+      c("blank", "character", "factor", "logical", "integer", "numeric", "Date", "text", "date", "skip"),
+      c(1:9, 1)
+    )
+  )
+})
+
+test_that('sanitize2 works', {
+  expect_equal(
+    sanitize2('$\\\\text[lw=.5]{hey}|u| % 4 & _#^~$'),
+    "\\$$\\backslash$$\\backslash$text\\lbracklw=.5\\rbrack\\{hey\\}$|$u$|$ \\% 4 \\& \\_\\#\\verb|^|\\~{}\\$"
+  )
+})
