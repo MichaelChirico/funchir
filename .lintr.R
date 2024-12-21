e27042d0fb3df1cae4cf6917bf08346b438c6c4d
Michael Chirico <michaelchirico4@gmail.com>
2024-12-21 22:16:37 +0800

    initialize settings

---
+++ .lintr.R
@@ -0,0 +1,18 @@
+linters = all_linters(
+  packages = "lintr",
+  implicit_integer_linter(allow_colon=TRUE),
+  line_length_linter(120L),
+  assignment_linter = NULL,
+  commas_linter = NULL,
+  commented_code_linter = NULL,
+  condition_call_linter = NULL,
+  infix_spaces_linter = NULL,
+  library_call_linter = NULL,
+  nonportable_path_linter = NULL,
+  object_name_linter = NULL,
+  quotes_linter = NULL,
+  spaces_inside_linter = NULL,
+  todo_comment_linter = NULL,
+  unreachable_code_linter = NULL
+)
+exclusions = list(`tests/testthat/stale_package_test_scripts/wont_parse.R` = Inf)
