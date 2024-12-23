d1c446d45f5a3c645ca69d02d847ba88e3db7b2e
Michael Chirico <michaelchirico4@gmail.com>
2024-12-23 16:18:29 +0800

    Remove sanitize2

--- tests/testthat/test-io-utils.R
+++
@@ -1,6 +0,0 @@
-test_that('sanitize2 works', {
-  expect_identical(
-    sanitize2('$\\\\text[lw=.5]{hey}|u| % 4 & _#^~$'),
-    "\\$$\\backslash$text\\lbracklw=.5\\rbrack\\{hey\\}$|$u$|$ \\% 4 \\& \\_\\#\\verb|^|\\~{}\\$"
-  )
-})
